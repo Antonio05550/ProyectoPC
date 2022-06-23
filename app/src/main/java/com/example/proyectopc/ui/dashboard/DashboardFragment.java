cebe5e5d1a64d7b3fdeed97f372513fa291b7b30
rivas <joanrivasna@ittepic.edu.mx>
2022-06-23 13:58:51 -0600

    origin

--- app/src/main/java/com/example/proyectopc/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/proyectopc/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.proyectopc.ui.dashboard;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +11,32 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.proyectopc.R;
 import com.example.proyectopc.databinding.FragmentDashboardBinding;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    // variable for our bar chart
+    BarChart barChart;
+
+    // variable for our bar data.
+    BarData barData;
+
+    // variable for our bar data set.
+    BarDataSet barDataSet;
+
+    // array list for storing entries.
+    ArrayList barEntriesArrayList;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +46,48 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // initializing variable for bar chart.
+        barChart = root.findViewById(R.id.idBarChart);
+
+        // calling method to get bar entries.
+        getBarEntries();
+
+        barDataSet = new BarDataSet(barEntriesArrayList, "Dias de la semana");
+
+        // creating a new bar data and
+        // passing our bar data set.
+        barData = new BarData(barDataSet);
+
+        // below line is to set data
+        // to our bar chart.
+        barChart.setData(barData);
+
+        // adding color to our bar data set.
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+
+        // setting text color.
+        barDataSet.setValueTextColor(Color.BLACK);
+
+        // setting text size
+        barDataSet.setValueTextSize(16f);
+        barChart.getDescription().setEnabled(false);
         return root;
+    }
+
+    private void getBarEntries() {
+        // creating a new array list
+        barEntriesArrayList = new ArrayList<>();
+
+        // adding new entry to our array list with bar
+        // entry and passing x and y axis value to it.
+        barEntriesArrayList.add(new BarEntry(1f, 5.30f));
+        barEntriesArrayList.add(new BarEntry(2f, 6.20f));
+        barEntriesArrayList.add(new BarEntry(3f, 5.15f));
+        barEntriesArrayList.add(new BarEntry(4f, 7));
+        barEntriesArrayList.add(new BarEntry(5f, 5.55f));
+        barEntriesArrayList.add(new BarEntry(6f, 6));
+        barEntriesArrayList.add(new BarEntry(7f, 5));
+
     }
 
     @Override
--- app/src/main/java/com/example/proyectopc/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/proyectopc/ui/home/HomeFragment.java
@@ -9,12 +9,33 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.proyectopc.R;
 import com.example.proyectopc.databinding.FragmentHomeBinding;
+import com.github.mikephil.charting.charts.BarChart;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    RecyclerView recyclerView, recyclerView2;
+
+    // Using ArrayList to store images data
+    ArrayList img = new ArrayList<>(Arrays.asList(R.drawable.icon1, R.drawable.icon2,
+            R.drawable.icon3, R.drawable.icon4));
+    ArrayList nombre = new ArrayList<>(Arrays.asList("TikTok", "Facebook", "Messenger", "WhatsApp"));
+    ArrayList tiempo = new ArrayList<>(Arrays.asList("50 m", "45 m", "30 m", "20 m"));
+
+    ArrayList img2 = new ArrayList<>(Arrays.asList(R.drawable.icon1, R.drawable.icon2,
+            R.drawable.icon3, R.drawable.icon4));
+    ArrayList nombre2 = new ArrayList<>(Arrays.asList("TikTok", "Facebook", "Messenger", "WhatsApp"));
+    ArrayList tiempo2 = new ArrayList<>(Arrays.asList("50 m", "45 m", "30 m", "20 m"));
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +45,34 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        recyclerView = root.findViewById(R.id.rViewTop);
+
+        // Setting the layout as linear
+        // layout for vertical orientation
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(root.getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        // Sending reference and data to Adapter
+        Adapter adapter = new Adapter(root.getContext(), img, nombre, tiempo);
+
+        // Setting Adapter to RecyclerView
+        recyclerView.setAdapter(adapter);
+
+        recyclerView2 = root.findViewById(R.id.recyclerView8);
+
+        // Setting the layout as linear
+        // layout for vertical orientation
+
+        LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(root.getContext());
+        recyclerView2.setLayoutManager(linearLayoutManager2);
+
+        // Sending reference and data to Adapter
+        Adapter adapter2 = new Adapter(root.getContext(), img, nombre, tiempo);
+
+        // Setting Adapter to RecyclerView
+        recyclerView2.setAdapter(adapter2);
+
         return root;
     }
 
